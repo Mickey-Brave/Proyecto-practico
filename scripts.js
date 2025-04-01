d346ca5466c86829e0765dbc907a7c51cdb00a8d
Mickey-Brave <curso1operaciones@gmail.com>
2025-04-01 17:45:48 +0200

    solucion-form

--- scripts.js
+++ scripts.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const nameForm = document.getElementById("nameForm");
     const nameInput = document.getElementById("name");
     const greeting = document.getElementById("greeting");
+    const formContainer = document.querySelector(".form_container"); 
 
     nameForm.addEventListener("submit", (event) => {
         event.preventDefault();
         const name = nameInput.value.trim();
         if (name) {
-            greeting.textContent = `Hello, ${name}! Welcome to the Image Gallery.`;
+            greeting.textContent = `Hola, ${name}! Bienvenido a la Galeria de Imagenes.`;
             nameForm.reset();
+            formContainer.style.display = "none";
         }
     });
 
